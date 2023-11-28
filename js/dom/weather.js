f473fef8c83374bf5f217898a28905033593d219
Shirasa P <shirasapraja@gmail.com>
2023-11-28 15:45:07 -0700

    update icon url

--- js/dom/weather.js
+++ js/dom/weather.js
@@ -19,7 +19,7 @@ const renderWeather = (weatherData, divElement) =>{
       <h5 class="card-title">${weatherData.name}, ${weatherData.sys.country}</h5>
       <h6 class="card-subtitle mb-2 text-muted">${weatherData.main.temp} °C</h6>
       <p class="card-text">${weatherData.weather[0].description}</p>
-      <img src="${weather.weather.icon}"/>
+      <img src="https://openweathermap.org/img/wn/${weather.weather.icon}@2x.png"/>
     </div>
   </div>`
 
